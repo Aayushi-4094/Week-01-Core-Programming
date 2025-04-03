e0f567bfe303f9a8ebba3ee519b8ebc83c66c7bc
Aayushi <aayushiagarwal4094@gmail.com>
2025-04-03 15:56:20 +0530

    Clean main branch

--- Day1/Level-1/Ques3.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner; 
-public class Ques3 {
-public static void main(String[] args) {
-double kilo = 10.8;
-double toMiles = kilo * 1.6;
-System.out.println("The distance " + kilo + "km in miles is " + toMiles);
- }
-}
